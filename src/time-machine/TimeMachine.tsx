d4823c170d9ec387cc726cfad6e3fca35893faaa
sam-ln <82455742+sam-ln@users.noreply.github.com>
2021-05-03 11:18:32 +0200

    Relocated function logic; Date slider ranges from earliest event to now; Fixed crash on invalid tags; Renamed npm commands

--- src/time-machine/TimeMachine.tsx
+++ src/time-machine/TimeMachine.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { OffsetMap, Reduce, Tags } from '@actyx/pond'
+import { OffsetMap, Pond, Reduce, Tags } from '@actyx/pond'
 import { usePond } from '@actyx-contrib/react-pond'
 
 const defaultOnEvent = `(state, event, metadata) => {
@@ -7,18 +7,18 @@ const defaultOnEvent = `(state, event, metadata) => {
 }`
 
 export const TimeMachine = (): JSX.Element => {
-  const [tags, setTags] = React.useState('error')
+  const [tags, setTags] = React.useState('oven-fish:oven_1')
   const [initState, setInitState] = React.useState('{"id": 1}')
-  const [onEventFunction, setOnEventFunction] = React.useState(defaultOnEvent)
+  const [onEventFunctionCode, setOnEventFunction] = React.useState(defaultOnEvent)
 
   const [upperBound, setUpperBound] = useState<OffsetMap>()
   const [startTime, setStartTime] = useState<number>()
-  const [currentTime, setCurrentTime] = useState<number>(0)
+  const [currentTime, setCurrentTime] = useState<number>(Date.now())
   const [selectedEventOffsetMap, setSelectedEventOffsetMap] = useState<OffsetMap>({})
 
   const [twinState, setTwinState] = useState({})
-
   const pond = usePond()
+
   React.useEffect(() => {
     const refresh = setInterval(() => {
       pond.events().currentOffsets().then(setUpperBound)
@@ -28,42 +28,49 @@ export const TimeMachine = (): JSX.Element => {
     }
   }, [])
 
-  const onEventFn = React.useCallback(
+  //Update Timespan for Slider each time the tags are changed
+  React.useEffect(() => {
+    console.log('Timecheck started')
+    console.log(tags)
+    if (!upperBound) {
+      return
+    }
+    const sidsWithOffsetsSetTo1 = sidsWithOffset1(upperBound)
+    console.log(sidsWithOffsetsSetTo1)
+    getEarliestEventMicros(pond, sidsWithOffsetsSetTo1, tags).then((earliestEventMicros) =>
+      setStartTime(earliestEventMicros),
+    )
+  }, [tags, upperBound])
+
+  const onEventFunction = React.useCallback(
     (state, event, meta): Reduce<unknown, unknown> => {
       try {
         return new Function(
           '__inState__',
           '__inEvent__',
           '__inMetadata__',
-          `try{ return (${onEventFunction})(__inState__, __inEvent__, __inMetadata__) } catch {return __inState__}`,
+          `try{ return (${onEventFunctionCode})(__inState__, __inEvent__, __inMetadata__) } catch {return __inState__}`,
         )(state, event, meta)
       } catch (error) {
         console.log(error)
         return state
       }
     },
-    [onEventFunction],
+    [onEventFunctionCode],
   )
 
+  //Reapply Events on Twin after Boundary Change
   React.useEffect(() => {
     if (!selectedEventOffsetMap) {
       return
     }
-    pond.events().queryKnownRangeChunked(
-      {
-        upperBound: selectedEventOffsetMap,
-        order: 'Asc',
-        query: Tags(...(tags || 'unknown').split(' ')),
-      },
-      5000,
-      ({ events }) => {
-        setTwinState(
-          events.reduce(
-            (state, { payload, meta }) => onEventFn(state, payload, meta),
-            JSON.parse(initState),
-          ),
-        )
-      },
+    reduceTwinStateFromEvents(
+      pond,
+      selectedEventOffsetMap,
+      tags,
+      onEventFunction,
+      initState,
+      setTwinState,
     )
   }, [selectedEventOffsetMap])
 
@@ -99,7 +106,7 @@ export const TimeMachine = (): JSX.Element => {
             <span>onEvent function:</span>
             <textarea
               style={{ width: 350 }}
-              value={onEventFunction}
+              value={onEventFunctionCode}
               onChange={({ target }) => setOnEventFunction(target.value)}
             />
           </div>
@@ -118,16 +125,18 @@ export const TimeMachine = (): JSX.Element => {
                 <input
                   style={{ width: 350 }}
                   type="range"
-                  min="1"
+                  min={0}
                   max={events}
                   value={value}
                   className="slider"
                   id="myRange"
                   onChange={({ target }) =>
-                    setSelectedEventOffsetMap({
-                      ...selectedEventOffsetMap,
-                      [sid]: +target.value,
-                    })
+                    addSourceToOffsetMap(
+                      setSelectedEventOffsetMap,
+                      selectedEventOffsetMap,
+                      sid,
+                      target,
+                    )
                   }
                 />
               </div>
@@ -143,7 +152,7 @@ export const TimeMachine = (): JSX.Element => {
             <input
               style={{ width: 350 }}
               type="range"
-              min={startTime || 0}
+              min={startTime ? startTime / 1000 : 0}
               max={Date.now()}
               value={currentTime}
               className="slider"
@@ -155,4 +164,74 @@ export const TimeMachine = (): JSX.Element => {
       </div>
     </div>
   )
+}
+function reduceTwinStateFromEvents(
+  pond: Pond,
+  selectedEventOffsetMap: { readonly [x: string]: number },
+  tags: string,
+  onEventFn: (state: any, event: any, meta: any) => Reduce<unknown, unknown>,
+  initState: string,
+  callback: (state: any) => void,
+): any {
+  pond.events().queryKnownRangeChunked(
+    {
+      upperBound: selectedEventOffsetMap,
+      order: 'Asc',
+      query: tagsFromString(tags),
+    },
+    5000,
+    ({ events }) => {
+      callback(
+        events.reduce((state, { payload, meta }) => {
+          return onEventFn(state, payload, meta)
+        }, JSON.parse(initState)),
+      )
+    },
+  )
+}
+
+function addSourceToOffsetMap(
+  setSelectedEventOffsetMap: React.Dispatch<React.SetStateAction<{ readonly [x: string]: number }>>,
+  selectedEventOffsetMap: { readonly [x: string]: number },
+  sid: string,
+  target: EventTarget & HTMLInputElement,
+): void {
+  return setSelectedEventOffsetMap({
+    ...selectedEventOffsetMap,
+    [sid]: +target.value,
+  })
+}
+
+function sidsWithOffset1(upperBound: { readonly [x: string]: number }) {
+  return Object.entries(upperBound).reduce((previous, [sid]) => ({ ...previous, [sid]: 1 }), {})
+}
+
+function getEarliestEventMicros(
+  pond: Pond,
+  offsets: {
+    readonly [x: string]: number
+  },
+  tags: string,
+): Promise<number> {
+  return pond
+    .events()
+    .queryKnownRange({
+      upperBound: offsets,
+      order: 'Asc',
+      query: tagsFromString(tags),
+    })
+    .then((events) =>
+      events.reduce((previous, current) =>
+        current.meta.timestampMicros < previous.meta.timestampMicros ? current : previous,
+      ),
+    )
+    .then((earliestEvent) => earliestEvent.meta.timestampMicros)
+}
+
+function tagsFromString(tags: string) {
+  try {
+    return Tags(...(tags || 'unknown').split(' '))
+  } catch (exception) {
+    return Tags('unknown')
+  }
 }
