1b15e6eecf48d53a951b3d030674bb7d3c6ed92a
sam-ln <82455742+sam-ln@users.noreply.github.com>
2021-05-19 15:23:25 +0200

    Added additional compareTimerange tests

--- test/time-machine/actyx-functions.test.ts
+++ test/time-machine/actyx-functions.test.ts
@@ -1,10 +1,30 @@
 import * as actyxFunc from '../../src/time-machine/actyx-functions'
 
-test('compares an earlier timestamp with a timerange', () => {
-  const earlyTimestamp = 10020
-  const lowerBound = 20000
-  const upperBound = 30000
-  expect(actyxFunc.compareTimestampWithTimeRange(earlyTimestamp, lowerBound, upperBound)).toBe(
-    'beforeRange',
-  )
+describe('Testing compareTimerange', () => {
+  test('compares an earlier timestamp with a timerange', () => {
+    const earlyTimestamp = 10020
+    const lowerBound = 20000
+    const upperBound = 30000
+    expect(actyxFunc.compareTimestampWithTimeRange(earlyTimestamp, lowerBound, upperBound)).toBe(
+      'beforeRange',
+    )
+  })
+
+  test('compares a later timestamp with a timerange', () => {
+    const laterTimestamp = 800000
+    const lowerBound = 20000
+    const upperBound = 30000
+    expect(actyxFunc.compareTimestampWithTimeRange(laterTimestamp, lowerBound, upperBound)).toBe(
+      'afterRange',
+    )
+  })
+
+  test('compares a timestamp that is inside the timerange with a timerange', () => {
+    const timestampWithinRange = 20500
+    const lowerBound = 20000
+    const upperBound = 30000
+    expect(
+      actyxFunc.compareTimestampWithTimeRange(timestampWithinRange, lowerBound, upperBound),
+    ).toBe('withinRange')
+  })
 })
