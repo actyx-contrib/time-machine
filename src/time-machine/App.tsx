163af839545f9f2f4223160fe5142619f98d771b
sam-ln <82455742+sam-ln@users.noreply.github.com>
2021-06-14 18:08:57 +0200

    Added checkbox lock

--- src/time-machine/App.tsx
+++ src/time-machine/App.tsx
@@ -50,9 +50,10 @@ export function App(): JSX.Element {
 
   const [selectedFishIndex, setSelectedFishIndex] = useState<number>(0)
   const [selectedTags, setSelectedTags] = React.useState(whereToTagsString(importedFishes[0].where))
-  const [selectedSyncCheckbox, setSelectedSyncCheckbox] = React.useState<{
+  const [selectedSyncCheckboxesMap, setSelectedSyncCheckbox] = React.useState<{
     readonly [x: string]: boolean
   }>({})
+  const [checkboxLock, setCheckboxLock] = useState<boolean>(false)
 
   const [calculatingOffsetLimits, setCalculatingOffsetLimits] = React.useState<boolean>(false)
   const [calculatingFishState, setCalculatingFishState] = React.useState<boolean>(false)
@@ -230,14 +231,15 @@ export function App(): JSX.Element {
                         sid={sid}
                         numberOfSelectedEvents={selectedEvents[sid] + 1 || 0}
                         numberOfAllEvents={events + 1}
-                        syncDisabled={false}
-                        syncSelected={selectedSyncCheckbox[sid] || false}
+                        syncDisabled={!selectedSyncCheckboxesMap[sid] && checkboxLock}
+                        syncSelected={selectedSyncCheckboxesMap[sid] || false}
                         onEventsChanged={(events) => {
                           setSelectedEvents(upsertOffsetMapValue(selectedEvents, sid, events - 1))
                         }}
-                        onSyncCheckboxChanged={(checked) =>
-                          setSelectedSyncCheckbox({ ...selectedSyncCheckbox, [sid]: checked })
-                        }
+                        onSyncCheckboxChanged={(checked) => {
+                          setSelectedSyncCheckbox({ ...selectedSyncCheckboxesMap, [sid]: checked })
+                          setCheckboxLock(checked)
+                        }}
                         disabled={calculatingFishState || calculatingOffsetLimits}
                         key={sid}
                       />
