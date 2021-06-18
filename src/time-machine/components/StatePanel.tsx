fafc18d793caf08d7ccee4c2b2e50efe1ea0af7c
sam-ln <82455742+sam-ln@users.noreply.github.com>
2021-06-18 16:55:33 +0200

    Code cleanup

--- src/time-machine/components/StatePanel.tsx
+++ src/time-machine/components/StatePanel.tsx
@@ -8,34 +8,6 @@ type StatePanelProps = {
 }
 
 export function StatePanel({ currentState, previousState }: StatePanelProps): JSX.Element {
-  const exampleJSON = {
-    quiz: {
-      sport: {
-        q1: {
-          question: 'Which one is correct team name in NBA?',
-          options: [
-            'New York Bulls',
-            'Los Angeles Kings',
-            'Golden State Warriros',
-            'Huston Rocket',
-          ],
-          answer: 'Huston Rocket',
-        },
-      },
-      maths: {
-        q1: {
-          question: '5 + 7 = ?',
-          options: ['10', '11', '12', '13'],
-          answer: '12',
-        },
-        q2: {
-          question: '12 - 8 = ?',
-          options: ['1', '2', '3', '4'],
-          answer: '4',
-        },
-      },
-    },
-  }
   return (
     <div>
       <Typography variant="h4" component="h4" className="sub-header" gutterBottom>
