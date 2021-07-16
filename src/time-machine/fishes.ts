746790950f01543faa7ac317ded4a32eb4cb233c
sam-ln <82455742+sam-ln@users.noreply.github.com>
2021-07-16 15:16:49 +0200

    Fixed exported fishes function

--- src/time-machine/fishes.ts
+++ src/time-machine/fishes.ts
@@ -2,6 +2,6 @@ import { Fish } from '@actyx/pond'
 
 import Fishes from './../../example-fishes'
 
-export default function fishes(): Fish<any, any>[] {
+export function fishes(): Fish<any, any>[] {
     return Fishes()
 }
